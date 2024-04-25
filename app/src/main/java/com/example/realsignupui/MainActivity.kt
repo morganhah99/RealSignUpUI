bbdf8a18eb2d498f9f80bc99479c16765b66bd1e
Morgan Hah <morganhah@MBPM2-FF4LQLHJR6-HAH.local>
2024-04-25 15:45:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/realsignupui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.realsignupui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.realsignupui.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+
+        binding.btnSubmitLogin.setOnClickListener{
+            val intent = Intent(this, SignedIn::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/realsignupui/SignedIn.kt
@@ -0,0 +1,28 @@
+package com.example.realsignupui
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.realsignupui.databinding.SignedInBinding
+import com.google.android.material.snackbar.Snackbar
+
+class SignedIn : AppCompatActivity() {
+    private lateinit var binding: SignedInBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = SignedInBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnSubmitLogin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
